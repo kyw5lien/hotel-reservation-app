66c4aebf217937012b570796591911f2ee2b08ab
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2023-04-04 20:49:44 +0000

    Update Customer.java: updating Javadoc.
    Update Customer.java: updating Javadoc.

--- hotel-reservation-app-master/src/model/Customer.java
+++ hotel-reservation-app-master/src/model/Customer.java
@@ -3,19 +3,19 @@ package model;
 import helperclasses.EmailValidator;
 
 /**
- *  Customer Account class: models information on a Hotel Customer Account; validating the customer account's e-mail included.
+ *  Customer class: models information of a Customer; validating the customer's e-mail included.
  * @author kyw5lien
  */
 public class Customer {
     private String firstName;
     private String lastName;
     private String email;
-    
+
     /**
-     * The constructor of the Customer Account class.
-     * @param firstName the customer account's first name.
-     * @param lastName  the customer account's last name.
-     * @param email     the provided e-mail for the customer account.
+     * The constructor of the Customer class.
+     * @param firstName the customer's first name.
+     * @param lastName  the customer's last name.
+     * @param email     the customer's provided e-mail.
      */
     public Customer(String firstName, String lastName, String email) {
         this.firstName = firstName;
@@ -24,13 +24,17 @@ public class Customer {
             this.email = email;
     }
 
+    /**
+     * A Getter Method to retrieve the Customer's e-mail.
+     * @return  the customer's e-mail.
+     */
     public String getEmail() {
         return email;
     }
 
     /**
-     * Provides information on the Customer's account.
-     * @return a String representing the Customer account's information.
+     * Provides a String object representing the Customer's information.
+     * @return a String representing the Customer's information.
      */
     @Override
     public String toString() {
