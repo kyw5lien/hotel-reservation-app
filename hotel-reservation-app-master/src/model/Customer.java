1143cc8d61a6ad012b6e6614cb8977caf56bff1d
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2023-04-04 19:31:14 +0000

    Deleting code on Customer.java
    Deleting code on Customer.java

--- hotel-reservation-app-master/src/model/Customer.java
+++ hotel-reservation-app-master/src/model/Customer.java
@@ -1,72 +1,34 @@
 package model;
 
 import helperclasses.EmailValidator;
-
 import java.util.InputMismatchException;
 
 /**
- *  Customer Account class: models information about the
- *  customer account and an e-mail validation.
- *
- * @author
+ *  Customer Account class: models information on a Hotel Customer Account; validating the customer account's e-mail included.
+ * @author kyw5lien
  */
 public class Customer {
-    // Fields.
     private String firstName;
     private String lastName;
     private String email;
-
-    // Constructors.
-    public Customer() {
-        // Empty constructor ...
-    }
-
+    
     /**
      * The constructor of the Customer Account class.
+     *
      * @param firstName the customer account's first name.
-     * @param lastName the customer account's last name.
-     * @param email the provided e-mail for the customer account.
+     * @param lastName  the customer account's last name.
+     * @param email     the provided e-mail for the customer account.
      */
     public Customer(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
-
-        if(EmailValidator.isValid(email))
+        if (EmailValidator.isValid(email))
             this.email = email;
     }
-
-    // Getters.
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    // Setters.
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        if(EmailValidator.isValid(email)) {
-            this.email = email;
-        } else {
-            throw new InputMismatchException();
-        }
-    }
-
-    // toString Method.
-
+    /**
+     * Provides information on the Customer's account.
+     * @return a String representing the Customer account's information.
+     */
     @Override
     public String toString() {
         return "Customer{" +
@@ -75,20 +37,4 @@ public class Customer {
                 ", email:'" + email + '\'' +
                 '}';
     }
-
-    /**
-     * Provides information about the Customer's account.
-     * @return a String representing the Customer account's information.
-     */
-    /*
-    @Override
-    public String toString() {
-        return "Customer " + "\n" +
-                "\t" +"--------------------------------------" +
-                "\n" +
-                "\t" + "First Name: " + firstName + "\n" +
-                "\t" + "Last Name: " + lastName + "\n" +
-                "\t" + "E-mail: " + email + "\n";
-    }
-    */
 }
